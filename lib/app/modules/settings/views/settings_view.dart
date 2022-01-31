032293df62305635d998ac2f5e418109fd5ab95b
manikanta <manikanta@genisists.com>
2022-01-31 14:00:15 +0530

    settings added

--- lib/app/modules/settings/views/settings_view.dart
+++ lib/app/modules/settings/views/settings_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:plantable/app/modules/settings/views/settings_items_view.dart';
+import 'package:plantable/base/constatnts/color_constants.dart';
 
 import '../controllers/settings_controller.dart';
 
@@ -8,6 +10,7 @@ class SettingsView extends GetView<SettingsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kAppBackgroundColor,
       appBar: AppBar(
         title: Text('Settings'),
         centerTitle: true,
@@ -18,9 +21,21 @@ class SettingsView extends GetView<SettingsController> {
         child: SingleChildScrollView(
           child: Column(
             children: [
+              SizedBox(
+                height: 20.0,
+              ),
               _profileItem(),
+              SizedBox(
+                height: 20.0,
+              ),
               _programItem(),
+              SizedBox(
+                height: 20.0,
+              ),
               _preferancesandNotificationItem(),
+              SizedBox(
+                height: 20.0,
+              ),
               _helpFeedbackAndAbout(),
             ],
           ),
@@ -30,14 +45,129 @@ class SettingsView extends GetView<SettingsController> {
   }
 
   Widget _profileItem() {
-    return Column(
-      children: [],
+    return SettingsItemsView(
+      suffexIcon: Icon(
+        Icons.account_circle_outlined,
+        size: 60.0,
+        color: kPrimaryColor,
+      ),
+      title: "Demo Account",
+      subtitle: 'Free Account',
+      from: 'PROFILE',
     );
   }
 
-  _programItem() {}
+  Widget _programItem() {
+    return Column(
+      children: [
+        SettingsItemsView(
+          suffexIcon: Icon(
+            Icons.book,
+            size: 30.0,
+            color: Colors.black38,
+          ),
+          title: "Program",
+          from: 'PROGRAM',
+          endText: "Reboot",
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 50.0),
+          child: Divider(
+            height: 0.05,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
 
-  _preferancesandNotificationItem() {}
+  _preferancesandNotificationItem() {
+    return Column(
+      children: [
+        SettingsItemsView(
+          suffexIcon: Icon(
+            Icons.book,
+            size: 30.0,
+            color: Colors.black38,
+          ),
+          title: "Unit Preferances",
+          endText: "Imperial",
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 50.0),
+          child: Divider(
+            height: 0.05,
+            color: Colors.black,
+          ),
+        ),
+        SettingsItemsView(
+          suffexIcon: Icon(
+            Icons.book,
+            size: 30.0,
+            color: Colors.black38,
+          ),
+          title: "Notificatios",
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 50.0),
+          child: Divider(
+            height: 0.05,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
 
-  _helpFeedbackAndAbout() {}
+  _helpFeedbackAndAbout() {
+    return Column(
+      children: [
+        SettingsItemsView(
+          suffexIcon: Icon(
+            Icons.book,
+            size: 30.0,
+            color: Colors.black38,
+          ),
+          title: "Help Center",
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 50.0),
+          child: Divider(
+            height: 0.05,
+            color: Colors.black,
+          ),
+        ),
+        SettingsItemsView(
+          suffexIcon: Icon(
+            Icons.book,
+            size: 30.0,
+            color: Colors.black38,
+          ),
+          title: "Feedback",
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 50.0),
+          child: Divider(
+            height: 0.05,
+            color: Colors.black,
+          ),
+        ),
+        SettingsItemsView(
+          suffexIcon: Icon(
+            Icons.book,
+            size: 30.0,
+            color: Colors.black38,
+          ),
+          title: "About",
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 50.0),
+          child: Divider(
+            height: 0.05,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    );
+  }
 }
